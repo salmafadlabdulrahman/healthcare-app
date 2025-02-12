1b68858d53a9b94668d5fd34c85952dc87c7172e
Salma <salmafadlabdulrahman@gmail.com>
2025-02-12 14:15:28 +0200

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +10,30 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green: {
+          500: "#24AE7C",
+          600: "#0D2A1F",
+        },
+        blue: {
+          500: "#79B5EC",
+          600: "#152432",
+        },
+        red: {
+          500: "#F37877",
+          600: "#3E1716",
+          700: "#F24E43",
+        },
+        light: {
+          200: "#E8E9E9",
+        },
+        dark: {
+          200: "#0D0F10",
+          300: "#131619",
+          400: "#1A1D21",
+          500: "#363A3D",
+          600: "#76828D",
+          700: "#ABB8C4",
+        },
       },
     },
   },
