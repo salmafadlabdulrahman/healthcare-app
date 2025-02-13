540d95de851aec0d985c74288b21cf4f7ed6c596
Salma <salmafadlabdulrahman@gmail.com>
2025-02-13 21:07:06 +0200

    add the radio button field

--- app/patientform/page.tsx
+++ app/patientform/page.tsx
@@ -31,6 +31,11 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import MuiFormControl from "@mui/material/FormControl";
+
 const formSchema = z.object({
   fullname: z.string().min(2, {
     message: "Username must be at least 2 characters.",
@@ -112,7 +117,7 @@ const PatientForm = () => {
                   )}
                 />
 
-                <div className="md:flex items-center gap-2">
+                <div className="md:flex items-center gap-2  ">
                   <FormField
                     control={form.control}
                     name="phonenumber"
@@ -164,31 +169,82 @@ const PatientForm = () => {
                   />
                 </div>
 
-                <div className="mt-[3em]">
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-full md:w-[280px] h-[45px] justify-start text-left font-normal bg-dark-400 hover:bg-dark-200 hover:text-white border-dark-500 text-white",
-                          !date && "text-muted-foreground"
-                        )}
+                <div className="mt-[3em] md:flex items-center justify-between gap-2">
+                  <div className="w-full md:w-[50%]">
+                    <p className="text-dark-600">Date of birth</p>
+                    <Popover>
+                      <PopoverTrigger asChild className="">
+                        <div className="">
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-full mt-[1em] h-[45px] justify-start text-left font-normal bg-dark-400 hover:bg-dark-200 hover:text-white border-dark-500 text-white",
+                              !date && "text-muted-foreground"
+                            )}
+                          >
+                            {" "}
+                            {/*[280px] */}
+                            <CalendarIcon className="mr-2 h-4 w-4" />
+                            {date ? (
+                              format(date, "PPP")
+                            ) : (
+                              <span className="text-dark-600 font-semibold">
+                                Select your birthdate
+                              </span>
+                            )}
+                          </Button>
+                        </div>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0 bg-dark-200 border-dark-500">
+                        <Calendar
+                          mode="single"
+                          selected={date}
+                          onSelect={setDate}
+                          initialFocus
+                          className="text-white"
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+
+                  <div className="mt-[3em] md:mt-1 md:w-[50%]">
+                    <MuiFormControl>
+                      <FormLabel
+                        id="demo-radio-buttons-group-label"
+                        className="text-dark-600"
                       >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date ? format(date, "PPP") : <span className="text-dark-600 font-semibold">Select your birthdate</span>}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0 bg-dark-200 border-dark-500">
-                      <Calendar
-                        mode="single"
-                        selected={date}
-                        onSelect={setDate}
-                        initialFocus
-                        className="text-white"
-                      />
-                    </PopoverContent>
-                  </Popover>
+                        Gender
+                      </FormLabel>
+                      <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="female"
+                        name="radio-buttons-group"
+                      >
+                        <div className="flex gap-2 mt-[1em] ml-[.8em]">
+                          <FormControlLabel
+                            value="female"
+                            control={<Radio />}
+                            label="Female"
+                            className="radio-btn"
+                          />
+                          <FormControlLabel
+                            value="male"
+                            control={<Radio />}
+                            label="Male"
+                            className="radio-btn"
+                          />
+                          <FormControlLabel
+                            value="other"
+                            control={<Radio />}
+                            label="Other"
+                            className="radio-btn"
+                          />
+                        </div>
+                      </RadioGroup>
+                    </MuiFormControl>
+                  </div>
                 </div>
+
                 <Button
                   type="submit"
                   className="bg-green-500 w-full mt-[3em] h-[40px]"
